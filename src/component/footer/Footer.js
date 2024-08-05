5d46634dca0901e275930d994999112fed270704
nanduanand1234 <155607976+nanduanand1234@users.noreply.github.com>
2024-08-05 21:38:29 +0530

    Update Footer.js

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -6,13 +6,7 @@ const Footer = () => {
     <>
       <footer>
         <div className='para'>
-          <p>© 2022 GorkCoder - All Rights Reserved.</p>
-        </div>
-        <div className='icon'>
-          <i className='fab fa-facebook-f'></i>
-          <i className='fab fa-instagram'></i>
-          <i className='fab fa-twitter'></i>
-          <i className='fab fa-youtube'></i>
+          <p>© 2024 Anand - All Rights Reserved.</p>
         </div>
       </footer>
     </>
